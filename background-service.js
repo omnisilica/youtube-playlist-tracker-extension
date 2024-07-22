9981ac7f8f9c0bb1186afa78bf87f51b4ff5d404
omnisilica <oxmaverix@gmail.com>
2024-07-22 16:14:36 -0400

    Added UI for when user is on YT page with active playlist. Comments of previous tries included.

--- background-service.js
+++ background-service.js
@@ -1,19 +1,89 @@
 (() => {
-  let portFromContentScript;
-  const connected = (port) => {
-    console.assert(port.name === "port-from-content-script");
-    portFromContentScript = port;
-    portFromContentScript.postMessage({ greeting: "hi there content script!" });
-    portFromContentScript.onMessage.addListener((message) => {
-      portFromContentScript.postMessage({
-        greeting: `In background script, received message from content script: ${message.greeting}`,
-      });
-    });
-  };
+  // let portFromContentScript;
+  // const connected = (port) => {
+  //   console.assert(port.name === "port-from-content-script");
+  //   portFromContentScript = port;
+  //   portFromContentScript.postMessage({ greeting: "hi there content script!" });
+  //   portFromContentScript.onMessage.addListener((message) => {
+  //     portFromContentScript.postMessage({
+  //       greeting: `In background script, received message from content script: ${message.greeting}`,
+  //     });
+  //   });
+  // };
 
-  chrome.runtime.onConnect.addListener(connected);
+  // chrome.runtime.onConnect.addListener(connected);
 
   /*chrome.browserAction.onClicked.addListener(() => {
     portFromContentScript.postMessage({ greeting: "they clicked the button!" });
   });*/
+
+  // let backgroundPort = chrome.runtime.connect({
+  //   name: "port-from-content-script",
+  // });
+
+  // backgroundPort.postMessage({ greeting: "hello from background service" });
+
+  // backgroundPort.onMessage.addListener((message) => {
+  //   console.log("In content script, received message from background script: ");
+  //   console.log(message.greeting);
+  // });
+
+  // document.body.addEventListener("click", () => {
+  //   backgroundPort.postMessage({ greeting: "they clicked the page!" });
+  // });
+
+  // chrome.runtime.onMessage.addListener((message, sender, response) => {
+  //   const { message } = message;
+
+  //   if (message === "check_playlist_recording_status") {
+  //     console.log("chrome runtime onMessage");
+  //     //Check whether video recording is true
+  //   }
+  // });
+
+  let portFromCS;
+
+  const checkPlaylistTrackingStatus = () => {
+    let playlistTrackingStatus;
+
+    if (chrome.storage.local.get([tabID])) {
+      //dsadf
+    }
+    return;
+  };
+
+  function connected(p) {
+    portFromView = p;
+    portFromView.postMessage({ greeting: "hi there content script!" });
+    portFromView.onMessage.addListener((message) => {
+      if (message.command === "check-playlist-tracking-status") {
+        //checkPlaylistTrackingStatus();
+        console.log(message.tabID);
+        chrome.storage.local.get(null).then((result) => {
+          var localStorageKeys = Object.keys(result);
+          if (!localStorageKeys.includes(message.tabID)) {
+            portFromView.postMessage({
+              purpose: "playlist-tracking-status",
+              trackingSatus: false,
+            });
+          }
+          //console.log(allKeys);
+        });
+        if (chrome.storage.local.get([message.command])) {
+          // chrome.storage.local.get([message.tabID]).then((result) => {
+          //   console.log("Checking playlist tracking status.");
+          //   let playlistTrackingObject = JSON.stringify(result);
+          //   console.log("The list so far", playlistTrackingObject);
+          // });
+        } else {
+          console.log("no");
+        }
+      }
+
+      portFromView.postMessage({
+        greeting: `In background script, received message from content script: ${message.command}`,
+      });
+    });
+  }
+  chrome.runtime.onConnect.addListener(connected);
 })();
--- view.js
+++ view.js
@@ -5,15 +5,10 @@ const currentTab = await getActiveTabURL();
 const currentTabParameters = currentTab.url.split("?")[1];
 const ytURLParameters = new URLSearchParams(currentTabParameters);
 const ytListParameter = ytURLParameters.get("list");
+const activeTab = await getActiveTabURL();
+const activeTabID = activeTab.id;
 
 const xhttp = new XMLHttpRequest();
-
-const returnAPIKey = (responseText) => {
-  //console.log("respoinse text: ", responseText);
-  let config = JSON.parse(responseText);
-  let API_KEY = config[0].YT_API_KEY;
-  return API_KEY;
-};
 
 const getApiKey = () => {
   return new Promise((resolve) => {
@@ -21,15 +16,6 @@ const getApiKey = () => {
     xhttp.open("GET", "config.json");
 
     xhttp.onload = () => {
-      // console.log(xhttp.responseText.config);
-      //console.log(xhttp.responseText);
-
-      //let config = JSON.parse(xhttp.responseText);
-
-      //returnAPIKey(xhttp.responseText);
-      //API_KEY = xhttp.responseText;
-      //console.log(API_KEY);
-      //return API_KEY;
       resolve(xhttp.responseText);
     };
 
@@ -38,89 +24,216 @@ const getApiKey = () => {
 };
 
 const requestPlaylistDetails = async () => {
-  console.log(
-    "API Key: ",
-    getApiKey().then((result) => {
-      console.log(result);
-    })
-  );
-  // const videoSnippetRequest = await fetch(
-  //   `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${ytListParameter}&key=${getApiKey()}`
+  // console.log(
+  //   "API Key: ",
+  //   getApiKey().then((result) => {
+  //     console.log(result);
+  //   })
   // );
 
-  //return await videoSnippetRequest.json();
+  return new Promise(
+    getApiKey().then((result) => {
+      let config = JSON.parse(result)[0];
+      console.log("The key: ", config.YT_API_KEY);
+
+      const videoSnippetRequest = fetch(
+        `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${ytListParameter}&key=${config.YT_API_KEY}`
+      );
+      console.log(videoSnippetRequest);
+    })
+  );
+
+  // const videoSnippetRequest = await fetch(
+  //   `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${ytListParameter}&key=${api_key}`
+  // );
+
+  //console.log(videoSnippetRequest);
+
+  // return videoSnippetRequest;
 };
 
-const getCurrentPlaylistThumbnail = () => {
-  return requestPlaylistDetails().items[0].snippet.thumbnails.default.url;
+// const getCurrentPlaylistThumbnail = () => {
+//   //console.log(requestPlaylistDetails());
+//   //return requestPlaylistDetails().items[0].snippet.thumbnails.default.url;
+
+//   return requestPlaylistDetails().then((result) => {
+//     console.log("see");
+//     console.log(result.json());
+//     return result.items[0].snippet.thumbnails.default.url;
+//   });
+// };
+
+// const getCurrentPlaylistTitle = () => {
+//   return requestPlaylistDetails().items[0].snippet.title;
+// };
+
+const renderNotOnYouTubePageUI = () => {
+  // Create elements
+  const not_a_yt_page_div = document.createElement("div");
+  const ytLinkAnchor = document.createElement("a");
+  const ytAnchorText = document.createTextNode("Go to YouTube");
+
+  // Add attributes
+  ytLinkAnchor.href = "https://www.youtube.com/";
+  ytLinkAnchor.title = "Click to navigate to YouTube.";
+  ytLinkAnchor.target = "_blank";
+
+  // Add classes
+  not_a_yt_page_div.className = "not-yt-page";
+
+  // Add text
+  not_a_yt_page_div.innerHTML =
+    "<p>It looks like you're not on a YouTube page.<p/>";
+
+  // Build Node structure (nest elements)
+  ytLinkAnchor.appendChild(ytAnchorText);
+  not_a_yt_page_div.appendChild(ytLinkAnchor);
+  viewRoot.appendChild(not_a_yt_page_div);
 };
 
-const getCurrentPlaylistTitle = () => {
-  return requestPlaylistDetails().items[0].snippet.title;
+const renderNoPlaylistFoundUI = () => {
+  // Create elements
+  const not_a_yt_page_with_active_playlist_div = document.createElement("div");
+  const not_a_yt_page_with_active_playlist_paragraph =
+    document.createElement("p");
+
+  // Add classes
+  not_a_yt_page_with_active_playlist_div.className =
+    "no-active-playlist-container";
+
+  // Add text
+  not_a_yt_page_with_active_playlist_paragraph.innerHTML =
+    "It looks like you're not on a YouTube page with an active playlist on it.";
+
+  // Build Node structure (nest elements)
+  not_a_yt_page_with_active_playlist_div.appendChild(
+    not_a_yt_page_with_active_playlist_paragraph
+  );
+  viewRoot.replaceChildren(not_a_yt_page_with_active_playlist_div);
 };
 
-const getCurrentPlaylistDetails = () => {
-  return {
-    thumbnail: getCurrentPlaylistThumbnail(),
-    title: getCurrentPlaylistTitle(),
-  };
+const getCurrentPlaylistDetails = async (api_key) => {
+  const playlistSnippetRequest = await fetch(
+    `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&id=${ytListParameter}&key=${api_key}`
+  );
+
+  const response = await playlistSnippetRequest.json();
+  // console.log(response);
+  return response;
+};
+
+const renderPlaylistFoundUI = (playlistTitle, thumbnailURL, trackingStatus) => {
+  // Create elements
+  const playlistDetails_section = document.createElement("section");
+  const playlistDetails_section_img = document.createElement("img");
+  const playlistDetails_sectionTitle_div = document.createElement("div");
+  const playlistDetails_sectionTitle_paragraph = document.createElement("p");
+  const record_button = document.createElement("button");
+  const playlistDetails_sectionRecordOptionDiv = document.createElement("div");
+  const playlistDetails_sectionRecordParagraph = document.createElement("p");
+
+  // Add attributes
+  playlistDetails_section_img.src = thumbnailURL;
+  //  playlistDetails_section_img.src = tempImg;
+  playlistDetails_section_img.alt = `Thumbnail image for ${playlistTitle}`;
+  playlistDetails_section_img.title = `Thumbnail image for ${playlistTitle}`;
+
+  // Add classes
+  playlistDetails_section.className = "playlist-details";
+  playlistDetails_sectionTitle_div.className = "playlist-title";
+  playlistDetails_sectionRecordOptionDiv.className = "record-playlist";
+  record_button.classList.add("record-button");
+
+  // Add text
+  playlistDetails_sectionTitle_paragraph.innerHTML = playlistTitle;
+  //playlistDetails_sectionTitle_paragraph.innerHTML = tempTitle;
+  playlistDetails_sectionRecordParagraph.innerHTML = "Record";
+
+  // Build Node structure (nest elements)
+  record_button.appendChild(playlistDetails_sectionRecordParagraph);
+
+  playlistDetails_sectionRecordOptionDiv.appendChild(record_button);
+
+  playlistDetails_sectionTitle_div.appendChild(
+    playlistDetails_sectionTitle_paragraph
+  );
+
+  playlistDetails_section.appendChild(playlistDetails_section_img);
+  playlistDetails_section.appendChild(playlistDetails_sectionTitle_div);
+  playlistDetails_section.appendChild(playlistDetails_sectionRecordOptionDiv);
+
+  viewRoot.appendChild(playlistDetails_section);
+
+  console.log("DOM title: ", title);
+  console.log("DOM thumbnail: ", thumbnailURL);
+  console.log("DOM status: ", trackingStatus);
+};
+
+const setPlaylistFoundUI = () => {
+  getApiKey().then(async (result) => {
+    let config = JSON.parse(result)[0];
+    //const activeTab = await getActiveTabURL();
+
+    let playlistDetailsPromise = await getCurrentPlaylistDetails(
+      config.YT_API_KEY
+    );
+    let playlistDetails = playlistDetailsPromise.items[0].snippet;
+
+    // chrome.tabs.sendMessage(
+    //   activeTab.id,
+    //   {
+    //     message: "check_playlist_recording_status",
+    //   },
+    //   renderPlaylistFoundUI
+    // );
+
+    let myPort = chrome.runtime.connect({
+      name: "port-from-background-service",
+    });
+
+    await myPort.postMessage({
+      command: "check-playlist-tracking-status",
+      tabID: activeTabID,
+    });
+
+    myPort.onMessage.addListener((message) => {
+      console.log("In view, received message from background script: ");
+      //console.log(message.greeting);
+      if (
+        message.purpose === "playlist-tracking-status" &&
+        !message.trackingSatus
+      ) {
+        console.log(message);
+        renderPlaylistFoundUI(
+          playlistDetails.title,
+          playlistDetails.thumbnails.default.url,
+          false
+        );
+      }
+    });
+
+    console.log(playlistDetails.title);
+    console.log(playlistDetails.thumbnails.default.url);
+    console.log("The key: ", config.YT_API_KEY);
+    console.log(getCurrentPlaylistDetails(config.YT_API_KEY));
+  });
 };
 
 (async () => {
-  //viewRoot.innerHTML = '<div class="title">lkThis is not a youtube video page.</div>';
   if (currentTab.url.includes("youtube.com/watch") && ytListParameter) {
     console.log("User is on a page with an active playlist.");
 
     //display current playlist
-    console.log("thumbnail: ", getCurrentPlaylistDetails().thumbnail);
-    console.log("thumbnail: ", getCurrentPlaylistDetails().title);
+    setPlaylistFoundUI();
   } else if (currentTab.url.includes("youtube.com")) {
     console.log(
       "User is on a YouTube page, but not one with an active playlist on it."
     );
 
-    // Create elements
-    const not_a_yt_page_with_active_playlist_div =
-      document.createElement("div");
-    const not_a_yt_page_with_active_playlist_paragraph =
-      document.createElement("p");
-
-    // Add classes
-    not_a_yt_page_with_active_playlist_div.className =
-      "no-active-playlist-container";
-
-    // Add text
-    not_a_yt_page_with_active_playlist_paragraph.innerHTML =
-      "It looks like you're not on a YouTube page with an active playlist on it.";
-
-    // Build Node structure (nest elements)
-    not_a_yt_page_with_active_playlist_div.appendChild(
-      not_a_yt_page_with_active_playlist_paragraph
-    );
-    viewRoot.replaceChildren(not_a_yt_page_with_active_playlist_div);
+    renderNoPlaylistFoundUI();
   } else {
     console.log("User is not on a page that's displaying a YouTube Playlist");
 
-    // Create elements
-    const not_a_yt_page_div = document.createElement("div");
-    const ytLinkAnchor = document.createElement("a");
-    const ytAnchorText = document.createTextNode("Go to YouTube");
-
-    // Add attributes
-    ytLinkAnchor.href = "https://www.youtube.com/";
-    ytLinkAnchor.title = "Click to navigate to YouTube.";
-    ytLinkAnchor.target = "_blank";
-
-    // Add classes
-    not_a_yt_page_div.className = "not-yt-page";
-
-    // Add text
-    not_a_yt_page_div.innerHTML =
-      "<p>It looks like you're not on a YouTube page.<p/>";
-
-    // Build Node structure (nest elements)
-    ytLinkAnchor.appendChild(ytAnchorText);
-    not_a_yt_page_div.appendChild(ytLinkAnchor);
-    viewRoot.appendChild(not_a_yt_page_div);
+    renderNotOnYouTubePageUI();
   }
 })();
